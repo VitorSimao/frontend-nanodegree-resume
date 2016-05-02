44663637c923ffe77a11184329e50ba22cf177d0
VitorSimao <victor.simon.dasilva@gmail.com>
2016-05-02 15:30:39 -0400

    practice

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,64 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Victor Simon";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+ var bio = {
+ 	name: "",
+ 	role: "",
+ 	contacts: {
+
+ 	},
+ 	skills: [],
+ 	bioPic: ""
+ }
+
+ var education = {
+ 	schools: [
+ 		{
+ 			name: "",
+ 			city: ""
+ 		},
+ 		{
+ 			name: "",
+ 			city: ""
+ 		}
+ 	],
+ 	onlineCourses: [
+ 		{
+
+ 		}
+ 	]
+ }
+
+ var work = {
+ 	jobs: [
+ 		{
+ 			employer: "",
+ 			title: ""
+ 		},
+ 		{
+ 			employer: "",
+ 			title: ""
+ 		}
+ 	]
+ }
+
+ var projects = {
+ 	projects: [
+ 		{
+ 			title: "",
+ 			dates: "",
+ 			description: "",
+ 			images: [
+
+ 			]
+ 		}
+ 	]
+ }
